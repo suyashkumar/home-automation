4c73c3ca5051ea8399009ce23cc71ebd3f9547bd
Suyash Kumar <suyash.kumar@duke.edu>
2017-01-08 13:48:39 -0800

    Added login API request/functionality

--- web-client/src/components/login.js
+++ web-client/src/components/login.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from '../axios-auth';
 import {Button} from 'react-toolbox/lib/button';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 import Input from 'react-toolbox/lib/input';
 import './login/login.css';
+
+const server = 'http://conduit.suyash.io';
 
 class Login extends Component {
 
@@ -13,6 +16,21 @@ class Login extends Component {
 
 	handleChange = (name, value) => {
 		this.setState({[name]: value});
+	}
+
+	handleLogin = () => {
+		axios.post(`${server}/api/auth`, {
+			email: this.state.email,
+			password: this.state.password
+		}).then(response => {
+			if(response.data.success) {
+				localStorage.setItem('jwtToken', response.data.token);
+				window.open('/#/', "_self");
+			} else {
+				console.log('Error!');
+			}
+		});
+	
 	}
 	
 	render() {
@@ -33,8 +51,11 @@ class Login extends Component {
 							name="password"
 							value={this.state.password}
 							onChange={this.handleChange.bind(this, 'password')}/>
-						<Button className="button" raised primary>
+						<Button className="button" onClick={this.handleLogin} raised primary>
 							Login
+						</Button>
+						<Button className="button" raised>
+							Register	
 						</Button>
 					</div>
 				</Card>
